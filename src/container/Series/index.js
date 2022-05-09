62af2680c2ef749acb9652dcdf8ed4f7fdc419f0
Ahmad <engr.ahmadmustafeen@gmail.com>
2022-05-09 19:02:52 +0500

    series component data rendered

--- src/container/Series/index.js
+++ src/container/Series/index.js
@@ -1,14 +1,80 @@
+import axios from "axios";
 import React from "react";
+import { Bars } from "react-loader-spinner";
+import Footer from "../../components/footer";
+import Header from "../../components/header";
 
-import './style.css';
+import "./style.css";
 
+const URL = "https://mumer01.github.io/publicApis/BoxOffice.json";
+const NUMBER_OF_MOVIES = 25;
+const fetchApiData = async () => {
+  return axios
+    .get(URL)
+    .then((data) => {
+      return data?.data?.entries;
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
 
 const Series = () => {
-    return (
-        <div className="SeriesContainer">
-        Series
-        </div>
-    );
-}
+  const [movies, setMovies] = React.useState([]);
+  const [displayData, setDisplayData] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
 
-export default Series
+  React.useEffect(() => {
+    fetchApiData()
+      .then((data) => {
+        setMovies(data);
+      })
+      .catch((err) => {
+        console.log(err, "err");
+      })
+      .finally(() => {
+        setTimeout(()=>{
+            setIsLoading(false);},1500);
+        })
+  }, []);
+  React.useEffect(() => {
+    setDisplayData(movies.slice(0, NUMBER_OF_MOVIES));
+  }, [movies]);
+  return (
+    <div className="moviesContainer">
+      <Header pageName="Popular Series" />
+
+      <div className="moviesListContainer">
+        {isLoading ? (
+          <div className="loader">
+          <Bars heigth="100" width="100" color="white" ariaLabel="loading-indicator" />
+          </div>
+        ) : (
+          <div className="moviesListWrapper">
+            {displayData.map((item, index) => {
+              return (
+                <div className="moviesListContainer-item" key={index}>
+                <div className="moviesListContainer-image">
+                <img src={item.images?.["Poster Art"]?.url} style={{width:"100%",height:"100%"}} />
+                </div>
+                <div className="moviesListContainer-desc">
+                 <h6>
+                 {item.title}
+                 </h6> 
+                 <h6>
+                  {item.releaseYear}
+                 </h6>
+                </div>
+                </div>
+              );
+            })}
+          </div>
+        )}
+      </div>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default Series;
