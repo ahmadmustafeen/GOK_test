7ff79fa3e5d69efa33cd05cd7a4fda34b52f444b
Rameez-28 <97097920+Rameez-28@users.noreply.github.com>
2022-05-09 16:52:35 +0500

    navigationTiles completed

--- src/container/Home/index.js
+++ src/container/Home/index.js
@@ -1,16 +1,34 @@
 import React from "react";
 import Header from "../../components/header";
-import './style.css';
+import "./style.css";
 
+const NavigationTile = ({ title }) => {
+  return (
+    <div className="navigationTilesContainer">
+      <div className="navigationTilesContainer-image" />
+      <div className="navigationTilesContainer-title">
+        <h2>{title}</h2>
+      </div>
+    </div>
+  );
+};
 
+const tiles = [{title:"Popular Serial"},{title:"Popular Movies"}];
 const Home = () => {
-    return (
-        <div>
-        <div className="headerContainer">
-            <Header />
-        </div>
-        </div>
-    );
-}
+  return (
+    <div>
+      <Header />
+      <div className="mainContainer">
+      {
+        tiles.map((item,index)=>{
+            return (
+                <NavigationTile title={item.title} key={index} />
+            )
+        })
+      }
+      </div>
+    </div>
+  );
+};
 
 export default Home;
